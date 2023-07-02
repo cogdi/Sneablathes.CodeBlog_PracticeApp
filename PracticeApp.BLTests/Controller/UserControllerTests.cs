8b69da02b5ff136f8877f41f82a53c55d9c7fb55
Sneablathes <hotpursit0603@gmail.com>
2023-07-02 23:53:27 +0600

    Module testing for User Controller has been added.

---
+++ PracticeApp.BLTests/Controller/UserControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticeApp.BL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeApp.BL.Controller.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        [TestMethod()]
+        public void SetNewUserDataTest()
+        {
+            // Arrange
+            var userName = Guid.NewGuid().ToString();
+            var dateOfBirth = DateTime.Now.AddYears(-18);
+            var weight = 80;
+            var height = 190;
+            var gender = "Male";
+
+            var controller = new UserController(userName);
+
+            // Act
+            controller.SetNewUserData(gender, dateOfBirth, weight, height);
+            var controller2 = new UserController(userName);
+
+            // Assert
+            Assert.AreEqual(userName, controller2.CurrentUser.Name);
+            Assert.AreEqual(dateOfBirth, controller2.CurrentUser.DateOfBirth);
+            Assert.AreEqual(weight, controller2.CurrentUser.Weight);
+            Assert.AreEqual(height, controller2.CurrentUser.Height);
+            Assert.AreEqual(gender, controller2.CurrentUser.Gender.Name);
+            Assert.IsFalse(controller2.IsNewUser);
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            // Arrange
+            var userName = Guid.NewGuid().ToString();
+
+            // Act
+            var controller = new UserController(userName);
+
+            // Assert
+            Assert.AreEqual(userName, controller.CurrentUser.Name);
+        }
+    }
+}
